eb3ba661f9ecfb39d12e847190de7a7965c02e60
Cory House <housecor@gmail.com>
2021-12-09 11:17:44 -0600

    Add form

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
 export function App() {
-  return <h1>Gymrat ??</h1>;
+  return (
+    <>
+      <h1>Gymrat</h1>
+      <form>
+        <div>
+          <label htmlFor="exercise">Exercise</label>
+          <br />
+          <input type="text" id="exercise" />
+        </div>
+
+        <div>
+          <label htmlFor="weight">Weight</label>
+          <br />
+          <input type="number" id="weight" />
+        </div>
+      </form>
+    </>
+  );
 }
