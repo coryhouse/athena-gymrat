3d278a7cfc5a6b09cc88a271a2a28c71dfdffec7
Cory House <housecor@gmail.com>
2021-12-09 14:01:34 -0600

    Add input value

--- src/reusable/Input.tsx
+++ src/reusable/Input.tsx
@@ -1,12 +1,18 @@
+export const inputType = ["text", "number", "email"] as const;
+type InputType = typeof inputType[number]; // This creates a union type
+
 type InputProps = {
   /** Input label */
   label: string;
 
   /** Input type */
-  type: "text" | "number" | "email";
+  type: InputType;
 
   /** Input ID */
   id: string;
+
+  /** Input value */
+  value: string;
 };
 
 /** Reusable Input with Label */
@@ -15,7 +21,7 @@ export function Input(props: InputProps) {
     <div>
       <label htmlFor={props.id}>{props.label}</label>
       <br />
-      <input type={props.type} id={props.id} />
+      <input type={props.type} id={props.id} value={props.value} />
     </div>
   );
 }
