ae62c348d314da1faf8a3b80cdc59e237faf2dfe
Cory House <housecor@gmail.com>
2021-12-09 14:01:18 -0600

    Add state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
 import { Input } from "./reusable/Input";
 
+type NewExercise = {
+  exercise: string;
+  weight: string;
+};
+
+const newExercise: NewExercise = {
+  exercise: "",
+  weight: "",
+};
+
 export function App() {
+  const [exercise, setExercise] = useState(newExercise);
+
   return (
     <>
       <h1>Gymrat</h1>
       <form>
-        <Input label="Exercise" id="exercise" type="text" />
-        <Input id="weight" label="Weight" type="number" />
+        <Input
+          value={exercise.exercise}
+          label="Exercise"
+          id="exercise"
+          type="text"
+        />
+        <Input
+          value={exercise.weight}
+          id="weight"
+          label="Weight"
+          type="number"
+        />
+        <input type="submit" value="Save Exercise" />
       </form>
     </>
   );
