9e40c9011efbc97fdb3a830887f455c89df4c8d4
Cory House <housecor@gmail.com>
2021-12-09 14:33:05 -0600

    Add onChange

--- src/reusable/Input.test.tsx
+++ src/reusable/Input.test.tsx
@@ -4,7 +4,9 @@ import { Input, inputType } from "./Input";
 
 it("should apply specified label and associate the label with the input", () => {
   const label = "Test label";
-  render(<Input id="test" label={label} type="text" value="" />);
+  render(
+    <Input onChange={() => {}} id="test" label={label} type="text" value="" />
+  );
   screen.getByLabelText(label);
 });
 
@@ -12,7 +14,13 @@ describe("should render with each supported input type", () => {
   inputType.forEach((inputType) => {
     it(inputType, () => {
       const { container } = render(
-        <Input id="test" label="Example" type={inputType} value="" />
+        <Input
+          onChange={() => {}}
+          id="test"
+          label="Example"
+          type={inputType}
+          value=""
+        />
       );
       expect(
         container.querySelector(`input[type="${inputType}"]`)
@@ -21,11 +29,9 @@ describe("should render with each supported input type", () => {
   });
 });
 
-// Exercise 2: Write a test that confirms the value
-// is set on the input when passed in.
 it("should apply the provided value to the input", () => {
   const { container } = render(
-    <Input id="test" label="" type="text" value="value" />
+    <Input onChange={() => {}} id="test" label="" type="text" value="value" />
   );
   expect(container.querySelector(`input[value="value"]`)).toBeInTheDocument();
 });
