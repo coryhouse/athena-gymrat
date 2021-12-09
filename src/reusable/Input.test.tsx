335af2f794174378e40dda9f7f043410e3637845
Cory House <housecor@gmail.com>
2021-12-09 14:17:18 -0600

    Fix tests

--- src/reusable/Input.test.tsx
+++ src/reusable/Input.test.tsx
@@ -4,7 +4,7 @@ import { Input, inputType } from "./Input";
 
 it("should apply specified label and associate the label with the input", () => {
   const label = "Test label";
-  render(<Input id="test" label={label} type="text" />);
+  render(<Input id="test" label={label} type="text" value="" />);
   screen.getByLabelText(label);
 });
 
@@ -12,11 +12,20 @@ describe("should render with each supported input type", () => {
   inputType.forEach((inputType) => {
     it(inputType, () => {
       const { container } = render(
-        <Input id="test" label="Example" type={inputType} />
+        <Input id="test" label="Example" type={inputType} value="" />
       );
       expect(
         container.querySelector(`input[type="${inputType}"]`)
       ).toBeInTheDocument();
     });
   });
+});
+
+// Exercise 2: Write a test that confirms the value
+// is set on the input when passed in.
+it("should apply the provided value to the input", () => {
+  const { container } = render(
+    <Input id="test" label="" type="text" value="value" />
+  );
+  expect(container.querySelector(`input[value="value"]`)).toBeInTheDocument();
 });
