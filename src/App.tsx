398a2350b0e17baf25791150cdcae9021b41e5bf
Cory House <housecor@gmail.com>
2021-12-09 14:57:05 -0600

    Add centralized onChange handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { Input } from "./reusable/Input";
 
 type NewExercise = {
@@ -14,18 +14,27 @@ const newExercise: NewExercise = {
 export function App() {
   const [exercise, setExercise] = useState(newExercise);
 
+  function onChange(event: ChangeEvent<HTMLInputElement>) {
+    setExercise({
+      ...exercise,
+      [event.target.id]: event.target.value,
+    });
+  }
+
   return (
     <>
       <h1>Gymrat</h1>
       <form>
         <Input
           value={exercise.exercise}
+          onChange={onChange}
           label="Exercise"
           id="exercise"
           type="text"
         />
         <Input
           value={exercise.weight}
+          onChange={onChange}
           id="weight"
           label="Weight"
           type="number"
