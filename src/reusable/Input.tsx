f9917947bff1bf4a98bf5ea5e70ff18b607292a5
Cory House <housecor@gmail.com>
2021-12-09 11:47:37 -0600

    Add comments

--- src/reusable/Input.tsx
+++ src/reusable/Input.tsx
@@ -1,9 +1,15 @@
 type InputProps = {
+  /** Input label */
   label: string;
+
+  /** Input type */
   type: "text" | "number" | "email";
+
+  /** Input ID */
   id: string;
 };
 
+/** Reusable Input with Label */
 export function Input(props: InputProps) {
   return (
     <div>
