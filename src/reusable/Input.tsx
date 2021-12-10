44e9cd31d76cb75a24514848fd2839a611789376
Cory House <housecor@gmail.com>
2021-12-10 15:00:49 -0600

    Track touched

--- src/reusable/Input.tsx
+++ src/reusable/Input.tsx
@@ -1,6 +1,8 @@
-import { ChangeEventHandler, FocusEventHandler } from "react";
+import { ChangeEventHandler, FocusEventHandler, useState } from "react";
+import { FormStatus } from "../types";
 
 export const inputType = ["text", "number", "email"] as const;
+
 type InputType = typeof inputType[number]; // This creates a union type
 
 type InputProps = {
@@ -23,25 +25,35 @@ type InputProps = {
   onChange: ChangeEventHandler<HTMLInputElement>;
 
   /** Function called on input blur */
-  onBlur: FocusEventHandler<HTMLInputElement>;
+  onBlur?: FocusEventHandler<HTMLInputElement>;
+
+  /** Status of the input's form */
+  formStatus: FormStatus;
 };
 
 /** Reusable Input with Label */
 export function Input(props: InputProps) {
+  const [touched, setTouched] = useState(false);
+
   return (
     <>
       <div>
         <label htmlFor={props.id}>{props.label}</label>
         <br />
         <input
-          onBlur={props.onBlur}
+          onBlur={(event) => {
+            setTouched(true);
+            if (props.onBlur) props.onBlur(event);
+          }}
           onChange={props.onChange}
           type={props.type}
           id={props.id}
           value={props.value}
         />
       </div>
-      {props.error && <p style={{ color: "red" }}>{props.error}</p>}
+      {props.error && (props.formStatus === "Submitted" || touched) && (
+        <p style={{ color: "red" }}>{props.error}</p>
+      )}
     </>
   );
 }
