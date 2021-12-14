b436ce130fdd8208577085fe337e8e52c1c5a5d4
Cory House <housecor@gmail.com>
2021-12-14 13:09:44 -0600

    Handle loading state

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,13 @@ import { Exercise } from "./types";
 
 export function App() {
   const [exercises, setExercises] = useState<Exercise[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       const _exercises = await getExercises();
       setExercises(_exercises);
+      setIsLoading(false);
     }
     fetchData();
   }, []);
@@ -28,16 +30,21 @@ export function App() {
           </li>
         </ul>
       </nav>
-      <Routes>
-        <Route path="/" element={<Exercises exercises={exercises} />} />
-        <Route
-          path="/add"
-          element={
-            <AddExercise exercises={exercises} setExercises={setExercises} />
-          }
-        />
-        <Route path="*" element={<h1>Page not found.</h1>} />
-      </Routes>
+
+      {isLoading ? (
+        "Loading..."
+      ) : (
+        <Routes>
+          <Route path="/" element={<Exercises exercises={exercises} />} />
+          <Route
+            path="/add"
+            element={
+              <AddExercise exercises={exercises} setExercises={setExercises} />
+            }
+          />
+          <Route path="*" element={<h1>Page not found.</h1>} />
+        </Routes>
+      )}
     </>
   );
 }
