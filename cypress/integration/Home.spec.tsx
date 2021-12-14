8edaf841079923607cbfd6ffb335d7d57d738de7
Cory House <housecor@gmail.com>
2021-12-14 14:22:28 -0600

    Rename

--- cypress/integration/Home.spec.tsx
+++
@@ -1,9 +0,0 @@
-describe("Home", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:3000");
-  });
-
-  it("should display the no exercises message when there are no exercises", () => {
-    cy.findByText("No exercises exist. :(");
-  });
-});
