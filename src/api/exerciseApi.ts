7c3a4098ed1c0eba861c9e09284914f0aa488c81
Cory House <housecor@gmail.com>
2021-12-14 11:12:30 -0600

    Add api function

---
+++ src/api/exerciseApi.ts
@@ -0,0 +1,15 @@
+import { NewExercise } from "../types";
+
+export async function addExercise(exercise: NewExercise) {
+  const resp = await fetch("http://localhost:3001/exercises", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(exercise),
+  });
+
+  if (!resp.ok) throw resp;
+  const savedExercise = await resp.json();
+  return savedExercise;
+}
