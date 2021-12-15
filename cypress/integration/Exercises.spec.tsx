0a60715907e45b91f2845484c0f4d898e65a8932
Cory House <housecor@gmail.com>
2021-12-15 13:57:21 -0600

    Add loading message test

--- cypress/integration/Exercises.spec.tsx
+++ cypress/integration/Exercises.spec.tsx
@@ -5,9 +5,13 @@ describe("Exercises", () => {
     cy.visit("http://localhost:3000");
   });
 
-  it("should display the 'no exercises' when there are no exercises", () => {
+  it("should display the loading message, then display the 'no exercises' message when there are no exercises", () => {
+    cy.findByText("Loading...");
     // "Log in" as the user with no exercises via the dev tools
     cy.setUser("0-exercises@nope.com");
     cy.findByText("No exercises exist. :(");
+
+    // Now the loading text should be hidden
+    cy.findByText("Loading").should("not.exist");
   });
 });
