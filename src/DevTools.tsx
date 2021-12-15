7f1d819c18488f4df9bdf517209b2e6c93587675
Cory House <housecor@gmail.com>
2021-12-15 09:35:38 -0600

    Add label

--- src/DevTools.tsx
+++ src/DevTools.tsx
@@ -3,7 +3,9 @@ import styles from "./DevTools.module.css";
 export function DevTools() {
   return (
     <section className={styles.root}>
-      <select>
+      <label htmlFor="devtools-user">User</label>
+      <br />
+      <select id="devtools-user">
         <option value="1">c@h.com</option>
         <option value="2">no-exercises@nope.com</option>
       </select>
