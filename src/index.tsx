6e56288fef457b22692ba02f43cc9ca477b810e8
Cory House <housecor@gmail.com>
2021-12-17 09:14:53 -0600

    Configure react-query

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,22 @@ import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
 import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function Fallback() {
   return <p>Oops, an error occurred.</p>;
 }
 
+const queryClient = new QueryClient();
+
 render(
   <ErrorBoundary FallbackComponent={Fallback}>
-    <BrowserRouter>
-      <ToastContainer />
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ToastContainer />
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </ErrorBoundary>,
   document.getElementById("root")
 );
